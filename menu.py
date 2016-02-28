7f4999f9cabbe89ec50766dd42f621f925f198d0
theMonarK <anthony_guillier@live.fr>
2016-02-28 15:05:16 +0100

    tkMessageBox update
    About, save, save as, save as & exit, load update

--- menu.py
+++ menu.py
@@ -11,7 +11,7 @@ class MenuBar(Frame):
     button_file = Menubutton(self,text="File")
     button_file.pack(side="left")
     menu_file=Menu(button_file)
-    menu_file.add_command(label='Save', command=self.saving(None))
+    menu_file.add_command(label='Save', command=self.saving)
     menu_file.add_command(label='Save as...', command=self.savingAs)
     menu_file.add_command(label ='Load', command=self.load)
     menu_file.add_command(label ='Exit', command=self.exit)
@@ -24,8 +24,7 @@ class MenuBar(Frame):
     button_help.configure(menu=menu_help)
 
   def about(self):
-      tkMessageBox.showinfo('Oscilloscope Simulator',
-                                 '\tAnthony Guillier\n a2guilli@enib.fr')
+      tkMessageBox.showinfo('Oscilloscope Simulator','\tAnthony Guillier\n \ta2guilli@enib.fr',icon='info')
 
   def error(self):
       errorTop=Toplevel()
@@ -36,85 +35,42 @@ class MenuBar(Frame):
       buttonOK.pack()
 
   def exit(self):
-      exitTop=Toplevel()
-      exitTop.title("Quit")
-      msg = Message(exitTop, text="Save ?")
-      msg.pack()
-      buttonNo = Button(exitTop, text="No", command=lambda: self.parent.destroy())
-      buttonNo.pack(side="left")
-      buttonSave = Button(exitTop, text="Save", command=lambda: self.savingExit(exitTop))
-      buttonSave.pack(side="left")
-      buttonSaveAs = Button(exitTop, text="Save as...", command=self.savingAsExit)
-      buttonSaveAs.pack(side="left")
-      buttonCancel = Button(exitTop, text="Cancel", command=exitTop.destroy)
-      buttonCancel.pack(side="left")
+      MsgBox=tkMessageBox.askquestion(icon='question',title="Quit",message="Save?",type='yesnocancel')
+      if MsgBox=='yes':
+          self.savingAsExit()
+      if MsgBox=='no':
+          self.parent.destroy()
 
   def load(self):
-      loadTop=Toplevel()
-      loadTop.title('Load...')
-      name=StringVar()
-      nameLabel = Label(loadTop, text = "Name :")
-      nameEntry = Entry(loadTop,textvariable=name)
-      nameLabel.grid(row=0)
-      nameEntry.grid(row=0,column=1)
-      buttonOk = Button(loadTop, text="OK", command=lambda: self.setParam(loadTop,name.get()))
-      buttonOk.grid(row=2,column=1)
-      buttonCancel = Button(loadTop, text="Cancel", command=loadTop.destroy)
-      buttonCancel.grid(row=2,column=2)
+      fileName = tkFileDialog.askopenfilename(parent=self.parent,title="Open...")
+      self.setParam(fileName)
 
   def test(self,nameEntry):
       print("enter")
 
-  def saving(self,toplevel,name='save'):
+  def saving(self,name='save'):
       save=shelve.open(name)
       save['amp']=str(self.subject.get_magnitude())
       save['freq']=str(self.subject.get_frequency())
       save['phase']=str(self.subject.get_phase())
       save.close()
-      if toplevel is None:
-          pass
-      else:
-          toplevel.destroy()
 
 
-  def savingExit(self,toplevel,name='save'):
-      self.saving(toplevel,name)
+  def savingExit(self,name='save'):
+      self.saving(name)
       self.parent.destroy()
 
   def savingAsExit(self):
-      saveAsTop=Toplevel()
-      saveAsTop.title("Save as...")
-      name=StringVar()
-      nameLabel = Label(saveAsTop, text = "Name :")
-      nameEntry = Entry(saveAsTop,textvariable=name)
-      nameLabel.grid(row=0)
-      nameEntry.grid(row=0,column=1)
-      buttonOk = Button(saveAsTop, text="OK", command=lambda: self.savingExit(saveAsTop,name=str(name.get())))
-      buttonOk.grid(row=2,column=1)
-      buttonCancel = Button(saveAsTop, text="Cancel", command=saveAsTop.destroy)
-      buttonCancel.grid(row=2,column=2)
+      fileName = tkFileDialog.asksaveasfilename(parent=self.parent,title="Save as...")
+      self.savingExit(fileName)
 
   def savingAs(self):
-      saveAsTop=Toplevel()
-      saveAsTop.title("Save as...")
-      name=StringVar()
-      nameLabel = Label(saveAsTop, text = "Name :")
-      nameEntry = Entry(saveAsTop,textvariable=name)
-      nameLabel.grid(row=0)
-      nameEntry.grid(row=0,column=1)
-      buttonOk = Button(saveAsTop, text="OK", command=lambda: self.saving(saveAsTop,name=str(name.get())))
-      buttonOk.grid(row=2,column=1)
-      buttonCancel = Button(saveAsTop, text="Cancel", command=saveAsTop.destroy)
-      buttonCancel.grid(row=2,column=2)
+      fileName = tkFileDialog.asksaveasfilename(parent=self.parent,title="Save as...")
+      self.saving(fileName)
 
-  def setParam(self,toplevel,name='save'):
-      if(os.path.exists(name)):
-          save=shelve.open(name)
-          print(save['amp'])
-          self.subject.set_magnitude(int(save['amp']))
-          self.subject.set_frequency(int(save['freq']))
-          self.subject.set_phase(int(save['phase']))
-          save.close()
-          toplevel.destroy()
-      else:
-          self.error()
+  def setParam(self,name='save'):
+      save=shelve.open(name)
+      self.subject.set_magnitude(int(save['amp']))
+      self.subject.set_frequency(int(save['freq']))
+      self.subject.set_phase(int(save['phase']))
+      save.close()
