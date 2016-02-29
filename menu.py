d44dcd84ef7572ee21cf7a679f37cffd8d30a156
theMonarK <anthony_guillier@live.fr>
2016-02-29 21:53:37 +0100

    create 'save.py'
    Update savings

--- menu.py
+++ menu.py
@@ -1,6 +1,5 @@
 from Tkinter import *
-import shelve
-import os.path
+from save import *
 import re,  tkFileDialog, tkMessageBox
 
 class MenuBar(Frame):
@@ -8,16 +7,17 @@ class MenuBar(Frame):
     Frame.__init__(self,borderwidth=2)
     self.parent = parent
     self.subject = subject
+    self.save = Save(self.parent,self.subject)
     self.button_file = Menubutton(self,text="File")
     self.button_file.pack(side="left")
     self.menu_file=Menu(self.button_file)
-    self.menu_file.add_command(label='Save', command=self.saving)
-    self.menu_file.add_command(label='Save as...', command=self.savingAs)
-    self.menu_file.add_command(label ='Load', command=self.load)
+    self.menu_file.add_command(label='Save', command=self.save.saving)
+    self.menu_file.add_command(label='Save as...', command=self.save.savingAs)
+    self.menu_file.add_command(label ='Load', command=self.save.load)
     self.menu_file.add_command(label ='Exit', command=self.exit)
     self.button_file.configure(menu=self.menu_file)
 
-    self.button_help = Menubutton(self,text="About")
+    self.button_help = Menubutton(self,text="Help")
     self.button_help.pack(side="left")
     self.menu_help=Menu(self.button_help)
     self.menu_help.add_command(label='About this application...', command=self.about)
@@ -29,42 +29,6 @@ class MenuBar(Frame):
   def exit(self):
       self.MsgBox= tkMessageBox.askquestion(title="Quit",message="Save?",type='yesnocancel')
       if self.MsgBox=='yes':
-          self.savingAsExit()
+          self.save.savingAsExit()
       if self.MsgBox=='no':
           self.parent.destroy()
-
-  def load(self):
-      self.fileName = tkFileDialog.askopenfilename(parent=self.parent,title="Open...")
-      self.setParam(self.fileName)
-
-  def test(self,nameEntry):
-      print("enter")
-
-  def saving(self,name='save'):
-      self.save=shelve.open(name)
-      self.save['amp']=str(self.subject.get_magnitude())
-      self.save['freq']=str(self.subject.get_frequency())
-      self.save['phase']=str(self.subject.get_phase())
-      self.save['offset']=str(self.subject.get_offset())
-      self.save.close()
-
-
-  def savingExit(self,name='save'):
-      self.saving(name)
-      self.parent.destroy()
-
-  def savingAsExit(self):
-      self.fileName = tkFileDialog.asksaveasfilename(parent=self.parent,title="Save as...")
-      self.savingExit(self.fileName)
-
-  def savingAs(self):
-      self.fileName = tkFileDialog.asksaveasfilename(parent=self.parent,title="Save as...")
-      self.saving(self.fileName)
-
-  def setParam(self,name='save'):
-      self.save=shelve.open(name)
-      self.subject.set_magnitude(float(self.save['amp']))
-      self.subject.set_offset(float(self.save['offset']))
-      self.subject.set_frequency(float(self.save['freq']))
-      self.subject.set_phase(float(self.save['phase']))
-      self.save.close()
---
+++ save.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from Tkinter import *
+import tkFileDialog, tkMessageBox, shelve
+
+class Save(object):
+    def __init__(self,parent,subject):
+        self.parent = parent
+        self.subject = subject
+
+    def load(self):
+        self.fileName = tkFileDialog.askopenfilename(parent=self.parent,title="Open...")
+        self.setParam(self.fileName)
+
+    def test(self,nameEntry):
+        print("enter")
+
+    def saving(self,name='save'):
+        self.save=shelve.open(name)
+        self.save['amp']=str(self.subject.get_magnitude())
+        self.save['freq']=str(self.subject.get_frequency())
+        self.save['phase']=str(self.subject.get_phase())
+        self.save['offset']=str(self.subject.get_offset())
+        self.save.close()
+
+
+    def savingExit(self,name='save'):
+        self.saving(name)
+        self.parent.destroy()
+
+    def savingAsExit(self):
+        self.fileName = tkFileDialog.asksaveasfilename(parent=self.parent,title="Save as...")
+        self.savingExit(self.fileName)
+
+    def savingAs(self):
+        self.fileName = tkFileDialog.asksaveasfilename(parent=self.parent,title="Save as...")
+        self.saving(self.fileName)
+
+    def setParam(self,name='save'):
+        self.save=shelve.open(name)
+        self.subject.set_magnitude(float(self.save['amp']))
+        self.subject.set_offset(float(self.save['offset']))
+        self.subject.set_frequency(float(self.save['freq']))
+        self.subject.set_phase(float(self.save['phase']))
+        self.save.close()
